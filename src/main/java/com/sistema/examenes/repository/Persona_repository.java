4fa9d5a6d14e54b2ce76f01667da97f1e94fbb2b
Saul <claudio.velecela.est@tecazuay.edu.ec>
2023-05-05 18:16:37 -0500

    servicios persona

--- src/main/java/com/sistema/examenes/repository/Persona_repository.java
+++ src/main/java/com/sistema/examenes/repository/Persona_repository.java
@@ -2,6 +2,9 @@ package com.sistema.examenes.repository;
 
 import com.sistema.examenes.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface Persona_repository extends JpaRepository<Persona, Long> {
+    @Query(value = "SELECT * FROM persona p JOIN usuarios u ON p.id_persona = u.persona_id_persona WHERE u.username = :username", nativeQuery = true)
+    public Persona obtenerPersona(String username);
 }
