a3bc58e2db963b21ca65fe132e604658455101df
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-05-07 18:45:02 -0500

    cambios

--- src/main/java/com/sistema/examenes/fenix/repository/DocentesfenixRepository.java
+++ src/main/java/com/sistema/examenes/fenix/repository/DocentesfenixRepository.java
@@ -4,17 +4,32 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sistema.examenes.fenix.entity.Docentesfenix;
 
 public interface DocentesfenixRepository extends JpaRepository<Docentesfenix, String> {
 
-    @Query("SELECT d FROM Docentesfenix d WHERE d.cedula = :cedula OR d.primer_apellido = :primer_apellido OR d.segundo_apellido = :segundo_apellido")
-    List<Docentesfenix> findByCedulaAndApellidos(String cedula, String primer_apellido, String segundo_apellido);
+    @Query("SELECT d FROM Docentesfenix d WHERE UPPER(d.primer_apellido) = UPPER(:primer_apellido)")
+    List<Docentesfenix> findByPrimer_Apellido(@Param("primer_apellido") String primer_apellido);
+
+    @Query("SELECT d FROM Docentesfenix d WHERE UPPER(d.segundo_apellido) = UPPER(:segundo_apellido)")
+    List<Docentesfenix> findBySegundo_Apellido(@Param("segundo_apellido") String segundo_apellido);
+
+    // metodo para buscar un docente por su segundo apellido
+    @Query("SELECT d FROM Docentesfenix d WHERE UPPER(d.segundo_apellido) = UPPER(:segundo_apellido) OR UPPER(d.primer_apellido) = UPPER(:primer_apellido)")
+    List<Docentesfenix> findByPrimer_ApellidoAndSegundo_Apellido(@Param("primer_apellido") String primer_apellido,
+            @Param("segundo_apellido") String segundo_apellido);
 
     // metodo para listar todos los docentes
     List<Docentesfenix> findAll();
 
     // metodo para buscar un docente por su id
     List<Docentesfenix> findByCedula(String cedula);
+
+    // // metodo para buscar un docente por su primer apellido
+    // List<Docentesfenix> findByPrimer_Apellido(String primer_apellido);
+
+    // // metodo para buscar un docente por su segundo apellido
+    // List<Docentesfenix> findBySegundoApellido(String segundo_apellido);
 }
