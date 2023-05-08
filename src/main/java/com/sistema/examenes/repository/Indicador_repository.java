b182c0a38718f53742e0d1e0d0574b5fbd12f49b
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-05-08 16:31:33 -0500

    cambios necesarios

--- src/main/java/com/sistema/examenes/repository/Indicador_repository.java
+++ src/main/java/com/sistema/examenes/repository/Indicador_repository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface Indicador_repository extends JpaRepository<Indicador, Long> {
-    @Query(value = "SELECT * from indicadores where visible =true",nativeQuery = true)
+    @Query(value = "SELECT * from indicadores where visible =true", nativeQuery = true)
     List<Indicador> listarIndicador();
+
+    // un query para buscar por id_subcriterio
+    @Query(value = "SELECT * from indicador where subcriterio_id_subcriterio = :id_subcriterio", nativeQuery = true)
+    List<Indicador> listarIndicadorPorSubcriterio(Long id_subcriterio);
+
 }
--- src/main/java/com/sistema/examenes/services/Subcriterio_Service.java
+++ src/main/java/com/sistema/examenes/services/Subcriterio_Service.java
@@ -1,11 +1,12 @@
 package com.sistema.examenes.services;
 
-
 import com.sistema.examenes.entity.Subcriterio;
 
 import java.util.List;
 
-public interface Subcriterio_Service extends GenericService<Subcriterio, Long>{
+public interface Subcriterio_Service extends GenericService<Subcriterio, Long> {
 
-    public List<Subcriterio> listar() ;
+    public List<Subcriterio> listar();
+
+    public List<Subcriterio> listarPorCriterio(Long id_criterio);
 }
