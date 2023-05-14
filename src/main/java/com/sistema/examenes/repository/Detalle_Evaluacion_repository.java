f46a2dc03de0074fdd3e7f08b879ab5b79dfa63a
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-05-14 15:25:31 -0500

    .

--- src/main/java/com/sistema/examenes/repository/Detalle_Evaluacion_repository.java
+++ src/main/java/com/sistema/examenes/repository/Detalle_Evaluacion_repository.java
@@ -10,4 +10,9 @@ import java.util.List;
 public interface Detalle_Evaluacion_repository extends JpaRepository<Detalle_Evaluacion, Long> {
     @Query(value = "SELECT * from detalle_evaluacion where visible =true",nativeQuery = true)
     List<Detalle_Evaluacion> listarDetalleEvaluacion();
+
+    @Query("SELECT d FROM Detalle_Evaluacion d WHERE d.visible = true AND d.evidencia.id_evidencia = :idEvidencia AND d.estado = false AND d.usuario.id = :idUsuario")
+    List<Detalle_Evaluacion> listarDetalleEvaluacionRechazada(Long idEvidencia, Long idUsuario);
+
+
 }
