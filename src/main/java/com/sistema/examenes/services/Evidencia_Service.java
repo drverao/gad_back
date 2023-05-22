a3439547bdd5783a2cceb7cc0c12f6edce5f8f25
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-05-21 22:50:30 -0500

    Fix asignación evidencia servicios

--- src/main/java/com/sistema/examenes/services/Evidencia_Service.java
+++ src/main/java/com/sistema/examenes/services/Evidencia_Service.java
@@ -9,7 +9,7 @@ public interface Evidencia_Service extends GenericService<Evidencia, Long> {
     public List<Evidencia> listar();
 
     public List<Evidencia> evidenciaUsuario(String username);
-    public List<Evidencia> listarEvidenciaAsigna() ;
+    public List<Evidencia> listarEvidenciaAsigna(Long idUsuario) ;
 
     List<Evidencia> listarEvidenciaPorIndicador(Long id_indicador);
     
