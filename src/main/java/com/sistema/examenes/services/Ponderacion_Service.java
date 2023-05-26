b348284219f17511fb94637e4affcdb43eee5bb6
ABYGAIL1410 <72097225+ABYGAIL1905@users.noreply.github.com>
2023-05-26 09:36:19 -0500

    Revert "Revert "ponderacion""
    This reverts commit 2d7a11a9a5c6b6bfed98a7da399905a9b7305ca4.

--- src/main/java/com/sistema/examenes/services/Ponderacion_Service.java
+++ src/main/java/com/sistema/examenes/services/Ponderacion_Service.java
@@ -2,9 +2,16 @@ package com.sistema.examenes.services;
 
 import com.sistema.examenes.entity.Ponderacion;
 
+import java.util.Date;
 import java.util.List;
 
-public interface Ponderacion_Service extends GenericService<Ponderacion, Long>{
-    public List<Ponderacion> listar() ;
+public interface Ponderacion_Service extends GenericService<Ponderacion, Long> {
+    public List<Ponderacion> listar();
+
+    public List<Date> listarPonderacionPorModelo(Long id_modelo);
+
+    public List<Ponderacion> listarPonderacionPorFecha(String fecha);
+
+    public List<Ponderacion> listarPorFecha(String fecha);
 
 }
