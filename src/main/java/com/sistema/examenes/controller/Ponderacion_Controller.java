ff6c0caf2f79efae1cbc755688e9e368a63b352f
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-05-31 11:29:25 -0500

    asteriscos

--- src/main/java/com/sistema/examenes/controller/Ponderacion_Controller.java
+++ src/main/java/com/sistema/examenes/controller/Ponderacion_Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/ponderacion")
 public class Ponderacion_Controller {
